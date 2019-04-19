76d77a3f15f5f641180aaae0823b89575547b3d5
jbone187 <jasen.green@gmail.com>
2019-04-19 11:37:41 -0700

    Function Added

--- process.py
+++ process.py
@@ -3,13 +3,17 @@ import re
 import sys
 
 
-proc = subprocess.check_output(['ps', 'aux'])
+def run_process():
 
-decData = proc.decode("utf8")
+    proc = subprocess.check_output(['ps', 'aux'])
+
+    decData = proc.decode("utf8")
+
+    if len(sys.argv) > 1:
+        string = re.findall(sys.argv[1], decData)
+        print(string)
+    else:
+        print("Please Add Argument")
 
 
-if len(sys.argv) > 1:
-    string = re.findall(sys.argv[1], decData)
-    print(string)
-else:
-    print("Please Add Argument")
+run_process()
